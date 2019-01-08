8b8c09f552a7ffe322a3e694585073cfddfdcb32
starzy <starzy@admin.com>
2019-01-08 20:47:58 +0800

    实现接收客户端数据并解析，利用反射机制调用本地类中方法并把结果回写给客户端

--- src/main/java/com/starzy/bio/server/BIOHandler.java
+++ src/main/java/com/starzy/bio/server/BIOHandler.java
@@ -1,10 +1,12 @@
 package com.starzy.bio.server;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.*;
+import java.lang.reflect.Method;
 import java.net.Socket;
 
+/**
+ * 使用BIO模拟RPC
+ */
 public class BIOHandler implements Runnable {
 
     private Socket socket;
@@ -18,13 +20,35 @@ public class BIOHandler implements Runnable {
     public void run() {
         try {
             InputStream inputStream = socket.getInputStream();
-
-
-
             OutputStream outputStream = socket.getOutputStream();
 
+            //客户端传入数据格式为---全类名：方法名：方法参数
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+            String readLine = bufferedReader.readLine();
+            String[] split = readLine.split(":");
+            String className = split[0];
+            String methodName = split[1];
+            String methodParam = split[2];
 
-        } catch (IOException e) {
+            //执行客户端调用方法
+            Class<?> forName = Class.forName(className);//根据全类名反射获取类的class文件
+            System.out.println("calling class:" + forName);
+            Object newInstance = forName.newInstance();//根据类文件实例化对象
+            Method method = forName.getMethod(methodName, String.class);//反射获取方法
+            System.out.println("calling method:" + method);
+            Object invoke = method.invoke(newInstance, methodParam);
+            System.out.println("result:" + invoke);
+
+            //把执行结果写回
+            PrintWriter pw = new PrintWriter(new BufferedOutputStream(outputStream));
+            pw.println(invoke);
+            pw.flush();
+            //释放资源
+            bufferedReader.close();
+            pw.close();
+            socket.close();
+
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
--- src/main/java/com/starzy/bio/server/BIOServer.java
+++ src/main/java/com/starzy/bio/server/BIOServer.java
@@ -4,6 +4,11 @@ import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 
+/**
+ * BIO服务器端
+ * 实现接受客户端数据，解析接收数据，
+ * 并使用反射技术调用对应的类中的方法，并把执行结果返回给客户端
+ */
 public class BIOServer {
 
     public static void main(String[] args) throws IOException {
