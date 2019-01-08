996987b36f7ef604b0f683f27d9b8884b3351770
starzy <starzy@admin.com>
2019-01-08 20:45:06 +0800

    实现BIO客户端，向服务器端发送数据

--- src/main/java/com/starzy/bio/client/BIOClient.java
+++ src/main/java/com/starzy/bio/client/BIOClient.java
@@ -1,15 +1,33 @@
 package com.starzy.bio.client;
 
-import java.io.IOException;
-import java.net.InetSocketAddress;
+import java.io.*;
 import java.net.Socket;
 
+/**
+ * BIO客户端，实现调用服务端类的方法
+ * 发送数据格式为全类名：方法名：参数值，
+ * 服务端返回执行方法结果
+ */
 public class BIOClient {
 
     public static void main(String[] args) throws IOException {
 
-        Socket socket = new Socket();
-        socket.connect(new InetSocketAddress("127.0.0.1",2019));
+        Socket socket = new Socket("127.0.0.1",2019);
+        OutputStream outputStream = socket.getOutputStream();
+        InputStream inputStream = socket.getInputStream();
+
+        PrintWriter pw = new PrintWriter(new BufferedOutputStream(outputStream));
+        pw.println("com.starzy.bio.server.TestBusiness:getPrice:yifu");
+        pw.flush();
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(inputStream));
+        String readLine = br.readLine();
+
+        System.out.println("client get result: " + readLine);
+
+        inputStream.close();
+        outputStream.close();
+        socket.close();
 
     }
 }
